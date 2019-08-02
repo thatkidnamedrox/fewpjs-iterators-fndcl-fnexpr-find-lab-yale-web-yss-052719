77210837a2aae18d6d7b3857f30c1fbab194f9a2
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-02 22:06:12 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let r = record.find(r => { return r.result === "W" })
+  if (r) {
+    return r.year
+  }
+  return r
+}
